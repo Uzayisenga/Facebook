6d025892a8025c969e9c1c93e72b7c22e5b8c89f
jeanarm <nizigiarmel2015@gmail.com>
2019-09-03 10:16:56 +0200

    Second commit of collections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
-
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
-
-  # GET /users/1/edit
   def edit
   end
 
@@ -50,9 +41,6 @@ class UsersController < ApplicationController
       end
     end
   end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
@@ -62,13 +50,11 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_user
       @user = User.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
+  def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
